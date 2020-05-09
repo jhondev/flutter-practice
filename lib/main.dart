7e540544ec8b8283f30414da0029e3cf19d6b973
Jhonattan <jma9974@gmail.com>
2020-05-08 23:21:33 -0500

    my card

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,81 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
+
+final Color appColor = Colors.teal;
+final Color lightColor = Colors.teal.shade100;
+final Color textColor = Colors.teal.shade900;
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Random Word Generator', home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+    return CupertinoApp(
+      home: Scaffold(
+        backgroundColor: appColor,
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                  radius: 70,
+                  backgroundImage: AssetImage("assets/images/my_avatar.png")),
+              Text(
+                "Jhondev",
+                style: TextStyle(
+                    fontFamily: "Pacifico",
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 55),
+              ),
+              Text("SOFTWARE DEVELOPER",
+                  style: TextStyle(
+                    fontFamily: "Source Sans Pro",
+                    color: lightColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 23,
+                  )),
+              Divider(
+                indent: 100,
+                endIndent: 100,
+                thickness: 1.2,
+                color: lightColor,
+              ),
+              IconTextBox(
+                icon: Icons.phone,
+                text: "+57 123 456 789",
+                color: textColor,
+              ),
+              IconTextBox(
+                icon: Icons.mail,
+                text: "jhondev@email.com",
+                color: textColor,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+class IconTextBox extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color color;
+  IconTextBox({Key key, this.icon, this.text, this.color}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Word Generator'),
+    return Card(
+      child: ListTile(
+        leading: Icon(
+          icon,
+          color: color,
+        ),
+        title: Text(text, style: TextStyle(color: color)),
       ),
-      body: _buildSuggestions(),
     );
   }
 }
