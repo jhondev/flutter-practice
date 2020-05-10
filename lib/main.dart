e9934b1a51c78ae559bfe4910c597c51f1c61fba
Jhonattan <jma9974@gmail.com>
2020-05-09 19:55:13 -0500

    dices

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,71 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-final Color appColor = Colors.teal;
-final Color lightColor = Colors.teal.shade100;
-final Color textColor = Colors.teal.shade900;
+final appColor = Colors.red;
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: appColor,
-        body: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                  radius: 70,
-                  backgroundImage: AssetImage("assets/images/my_avatar.png")),
-              Text(
-                "Jhondev",
-                style: TextStyle(
-                    fontFamily: "Pacifico",
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 55),
-              ),
-              Text("SOFTWARE DEVELOPER",
-                  style: TextStyle(
-                    fontFamily: "Source Sans Pro",
-                    color: lightColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 23,
-                  )),
-              Divider(
-                indent: 100,
-                endIndent: 100,
-                thickness: 1.2,
-                color: lightColor,
-              ),
-              IconTextBox(
-                icon: Icons.phone,
-                text: "+57 123 456 789",
-                color: textColor,
-              ),
-              IconTextBox(
-                icon: Icons.mail,
-                text: "jhondev@email.com",
-                color: textColor,
-              )
-            ],
-          ),
+        appBar: AppBar(
+          backgroundColor: appColor,
+          centerTitle: true,
+          title: Text("Dice"),
         ),
+        body: DicePage(),
       ),
     );
   }
 }
 
-class IconTextBox extends StatelessWidget {
-  final IconData icon;
-  final String text;
-  final Color color;
-  IconTextBox({Key key, this.icon, this.text, this.color}) : super(key: key);
+class DicePage extends StatefulWidget {
+  DicePage({Key key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var diceNumber = 1;
+  var dice2Number = 1;
+
+  int getDiceNumber() {
+    return Random().nextInt(6) + 1;
+  }
+
+  void changeDiceFace() {
+    setState(() {
+      diceNumber = getDiceNumber();
+      dice2Number = getDiceNumber();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: Icon(
-          icon,
-          color: color,
-        ),
-        title: Text(text, style: TextStyle(color: color)),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset("assets/images/dices/dice$diceNumber.png"),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset("assets/images/dices/dice$dice2Number.png"),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
